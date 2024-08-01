b49bd2213f99f4c4165189faf0eb68c245f6d407
tong <1922527051@qq.com>
2024-08-02 00:04:15 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,14 +1,8 @@
-from ultralytics import YOLOv10 as YOLO
-import os
+from ultralytics import YOLOv10
 
+model = YOLOv10('./best.pt')
+# or
+# wget https://github.com/THU-MIG/yolov10/releases/download/v1.1/yolov10{n/s/m/b/l/x}.pt
+# model = YOLOv10('yolov10{n/s/m/b/l/x}.pt')
 
-def predict_init(args):
-    global model
-    print("Predict on", os.path.normpath(args.model_dir + args.model), "(FP16)" if args.half else "(FP32)")
-    model = YOLO(args.model_dir + args.model)
-
-
-def predict(args,img):
-    if img is not None:
-        output = model(img, verbose=args.verbose, half=args.half, iou=args.iou, conf=args.conf)
-    return output[0]
+model.predict(source="C:\\Users\yao19\Downloads\yolov10\datasets\images\\train\\0a0b4e09cbd0d703fcb65143a3ed18cd.png", save=True, imgsz=640, conf=0.3)
